96bcce2a6507055ffc9e067848406d7f5286a245
Mihail Chaplygin <caplyginmihail48@gmail.com>
2023-09-13 21:40:54 +0300

    change welcome text

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ import ReactionHandler.ReactionHandler;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder.createDefault("MTE1MTI0ODM2ODQ1NTEzMTE2Ng.GUisQG.6CoRgi4N0UJ6zGUqHsHOmN-UlgCmstnswhzHMw")
+        JDA jda = JDABuilder.createDefault("MTE1MTI0ODM2ODQ1NTEzMTE2Ng.GUsih_.xaU00qLeNhWmulJ8AEBB8OPgbgUn5-ziFrai7c")
                 .enableIntents(GatewayIntent.GUILD_MESSAGES) // Для сообщений в серверных чатах
                 .setActivity(Activity.playing("Fight with shadow"))
                 .build();
@@ -25,7 +25,12 @@ public class Main {
         TextChannel textChannel = jda.getCategories().get(1).getTextChannels().get(0);
 
         // Отправляем сообщение и сохраняем его в переменной
-        Message message = textChannel.sendMessage("Hello").complete();
+        Message message = textChannel.sendMessage("На сервер поселелился ОТТ бот." +'\n'
+                                                    +"он подскажет как лучше распределить торговое время." +'\n'
+                                                    + "чтобы он упоминал тебя когда ты за чартами, отреагируй на это сообщение: " +'\n'
+                                                    + "LO - если торгуешь Лондон" +'\n'
+                                                    + "AM - если торгуешь утро Нью-Йорка" +'\n'
+                                                    + "PM -  если мучаешь вечернюю Нью-Йоркскую сессию").complete();
 
         // Добавляем обработчик реакций для этого сообщения
         jda.addEventListener(new ReactionHandler(message));
